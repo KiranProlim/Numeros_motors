9df7b1c96c36afd1969cadb84ecb1fede0f48056
KiranProlim <kiranmahendrakar159k@gmail.com>
2026-08-12 12:06:34 +0530

    Initial Spring Boot PostgreSQL Project

---
+++ src/main/java/com/prolim/numerosmotors/NumerosmotorsApplication.java
@@ -0,0 +1,13 @@
+package com.prolim.numerosmotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerosmotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerosmotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prolim/numerosmotors/NumerosmotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolim.numerosmotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerosmotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
