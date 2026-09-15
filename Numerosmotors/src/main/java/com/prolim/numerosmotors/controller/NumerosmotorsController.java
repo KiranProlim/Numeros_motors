1fdb18b22ab09a0ee4ff7134cf3a703ed6c895a3
KiranProlim <kiranmahendrakar159k@gmail.com>
2026-09-15 18:38:01 +0530

    DEMO-395 - TASK-001 Configure Agile Template

--- Numerosmotors/src/main/java/com/prolim/numerosmotors/controller/NumerosmotorsController.java
+++ Numerosmotors/src/main/java/com/prolim/numerosmotors/controller/NumerosmotorsController.java
@@ -20,7 +20,7 @@ public class NumerosmotorsController {
 	private NumerosmotorsService service;
 
 	//DEMO-395 - TASK-001 Configure Agile Template	................................................
-	//hai
+	
 	@GetMapping("/home")
 	public String home() {
 		return "home";
